6bb65af4aa1b34de6f88eacd7b4b077cd8900b7a
TrevorHarron <trharron@gmail.com>
2014-09-28 11:35:49 -0700

    refactor to include more than one argument for a fuction

--- src/basic/Ops.scala
+++ src/basic/Ops.scala
@@ -1,5 +1,5 @@
 package basic
-class Ops(val flag: String,val description: String, val func: (String) => Unit ) {
+class Ops(val flag: String,val description: String, val func: (Array[String]) => Unit ) {
   
   def func_(){}
   def flag_(){}
